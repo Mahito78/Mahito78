d3aae7eefb4e4838f54616cc3354c34e33bce988
Mahito78 <9538453+Mahito78@users.noreply.github.com>
2023-06-30 00:44:03 +0200

    add img folder

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     var valeurElement = document.getElementById('valeur-score');
     var coinElement = document.getElementById('valeur-coin');
+    var timeElement = document.getElementById('valeur-time');
     var coin = 0;
+    var timing = 369
+
+    function decrementer(){
+        timing-=1;
+        timeElement.innerText = timing;
+    }
+
     function incrementer() {
       var date = new Date();
       var heure = date.getHours();
@@ -10,6 +18,9 @@ document.addEventListener('DOMContentLoaded', function() {
       var coin_rand = Math.random()*100%10;
       if(coin_rand >0){
         coin +=1;
+        if(coin%30 == 0){
+            coin = 0;
+        }
         coinElement.innerText = coin;
       }
       var valeur = val; // Incrémente de 0 à 11
@@ -19,7 +30,9 @@ document.addEventListener('DOMContentLoaded', function() {
     
     // Appel initial de la fonction
     incrementer();
-    
+    decrementer();
     // Appel répété de la fonction toutes les secondes
     setInterval(incrementer, 1000);
+    setInterval(decrementer, 1000);
+
   });
